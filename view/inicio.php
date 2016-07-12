78af87ccbbae30cf312309ca5f3e81f6a0ec5498
Ángela Escobar <angelaescobarmimbrera@MacBook-Pro-de-Angela.local>
2016-07-12 20:27:41 +0200

    poca cosa
    poca cosa

--- view/inicio.php
+++ view/inicio.php
@@ -89,7 +89,7 @@
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Configuración<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-        			   <li><a data-toggle="modal" href="#cambiosPass">Cambiar contraseña</a></li>
+                    <li><a data-toggle="modal" href="#cambiosPass">Cambiar contraseña</a></li>
                     <!--<li><a href="#">Another action</a></li>
                     <li><a href="#">Something else here</a></li>-->
                   <li class="divider"></li>
